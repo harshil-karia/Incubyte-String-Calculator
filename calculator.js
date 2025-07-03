57b0bfb229ddc8abbed64cf4596938ed216d3938
Harshil Karia <harshil.karia.hk@gmail.com>
2025-07-03 22:32:43 +0530

    Added test when only one number is passed

--- calculator.js
+++ calculator.js
@@ -1,6 +1,11 @@
 function add(numbers) {
   if (numbers.length == 0) {
+    // If no number is passed as input then returning 0
     return 0;
+  } else {
+    //Else returning value when only 1 number is passed
+    const value = parseInt(numbers);
+    return value;
   }
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,13 @@
 const { add } = require("./calculator");
 
+//Test Case for empty string
 test("returns 0 from the empty string", () => {
   expect(add("")).toBe(0);
+});
+
+//Test Case for string with single input
+test ("return the only number when one number is entered", () => {
+    expect(add("2")).toBe(2);
+    expect(add("3")).toBe(3);
+    expect(add("100")).toBe(100);
 });
